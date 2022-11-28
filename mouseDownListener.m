250f69411a2d572a4ec249b93faa3f29a12dd0b7
contessa-hdl <116019666+contessa-hdl@users.noreply.github.com>
2022-11-28 09:27:15 -0500

    Ahmad's changes
    run these on school computer and make sure they work. Keeping separate from original files until double checked.

--- mouseDownListener.m
+++ mouseDownListener.m
@@ -2,4 +2,6 @@ function mouseDownListener(src, event)
     %no body.
     %this handler must be registerd for axis 'CurrentPoint' to be
     %updated when mouse moves.
+    global clickedDown
+    clickedDown = true;
 end
